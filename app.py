a6ff241e5d531ea64bafceafc7843e54ada1c30b
marcello prado <marcellopradomuller@gmail.com>
2025-08-20 11:36:55 -0300

    Separando base css do home css

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def projeto_detalhe(id):
     return render_template("projetoViews.html", projeto=projeto)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
     
     #^ Para inicializar o ambiente virtual - venv\Scripts\activate
 #^  Arquivo principal do Flask para rodar o portfólio - python app.py
